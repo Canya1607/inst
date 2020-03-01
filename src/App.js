08a4b1800cb53d8bcdc7053ddfd22b097deb70c3
Canya1607 <hnenniy1610@gmail.com>
2020-03-02 01:07:54 +0200

    Added @material-ui and simple redesign of main page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Grid, List, ListItem, Link } from '@material-ui/core';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="root-container">
+        <Grid container className="header-container" justify="center" alignItems="center">
+          <p>INST ®</p>
+        </Grid>
+        <Grid container className="body-container" justify="center">
+          <List>
+            <ListItem className="list-item">
+              <p>Olexandr Hnennyi</p>
+            </ListItem>
+            <ListItem className="list-item">
+              <p>Vlados Bobrovskyi</p>
+            </ListItem>
+            <ListItem className="list-item">
+              <p>Maxim Potapov</p>
+            </ListItem>
+          </List>
+        </Grid>
+        <Grid container className="footer-container" justify="center">
+          <p>Reserved in Lviv - 2020</p>
+        </Grid>
       </div>
     );
   }
